d08934b3de673a468df69216d29cb8089a963895
Никита Водеников <VodenikovNI@mosreg.ru>
2026-06-30 19:10:20 +0300

    feat: redesign index page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,7 +5,14 @@ import { Link } from "@tanstack/react-router";
 import { useAuth } from "@/lib/contexts/auth-context";
 import { useIsMobile } from "@/hooks";
 import { useTabbar } from "@/lib/contexts/tabbar-context";
-import { MenuIcon, UsersIcon } from "lucide-react";
+import {
+  CalendarDaysIcon,
+  CarIcon,
+  CreditCardIcon,
+  HeartHandshake,
+  MenuIcon,
+  UsersIcon,
+} from "lucide-react";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -17,8 +24,8 @@ function Index() {
   const { setOpen } = useTabbar();
 
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-      <Card className="col-span-3 rounded-2xl shadow-xl">
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-12">
+      <Card className="col-span-12 rounded-2xl shadow-xl">
         <CardHeader>
           <CardTitle>Добро пожаловать</CardTitle>
         </CardHeader>
@@ -29,7 +36,7 @@ function Index() {
         </CardContent>
       </Card>
 
-      <Link to="/users" className="col-span-2">
+      <Link to="/users" className="col-span-6">
         <Card className="rounded-2xl shadow-xl relative overflow-hidden">
           <CardHeader>
             <CardTitle className="">Пользователи</CardTitle>
@@ -39,9 +46,88 @@ function Index() {
           <div className="absolute top-3 right-3">
             <div className="relative flex h-20 w-20 items-center justify-center">
               <div className="absolute inset-0 rounded-full bg-blue-300/30 animate-[pulse-ring-expand_2s_ease-in-out_infinite]" />
-              <div className="absolute inset-0 rounded-full bg-blue-300/30 animate-[pulse-ring-expand_2s_ease-in-out_infinite_1s]" />
+              <div className="absolute inset-0 rounded-full bg-blue-300/20 animate-[pulse-ring-expand_2s_ease-in-out_infinite_1s]" />
               <div className="relative z-10 flex h-20 w-20 items-center justify-center rounded-full bg-blue-300/10">
                 <UsersIcon className="h-10 w-10 text-blue-300" />
+              </div>
+            </div>
+          </div>
+        </Card>
+      </Link>
+
+      <Link to="/clients" className="col-span-6">
+        <Card className="rounded-2xl shadow-xl relative overflow-hidden">
+          <CardHeader>
+            <CardTitle className="">Клиенты</CardTitle>
+            <CardDescription className="">Просмотр клиентов</CardDescription>
+          </CardHeader>
+
+          <div className="absolute top-3 right-3">
+            <div className="relative flex h-20 w-20 items-center justify-center">
+              <div className="absolute inset-0 rounded-full bg-yellow-300/30 animate-[pulse-ring-expand_2s_ease-in-out_infinite]" />
+              <div className="absolute inset-0 rounded-full bg-yellow-300/20 animate-[pulse-ring-expand_2s_ease-in-out_infinite_1s]" />
+              <div className="relative z-10 flex h-20 w-20 items-center justify-center rounded-full bg-yellow-300/10">
+                <HeartHandshake className="h-10 w-10 text-yellow-300" />
+              </div>
+            </div>
+          </div>
+        </Card>
+      </Link>
+
+      <Link to="/drivers" className="col-span-5">
+        <Card className="rounded-2xl shadow-xl relative overflow-hidden">
+          <CardHeader>
+            <CardTitle className="">Водители</CardTitle>
+            <CardDescription className="">Управление водителями</CardDescription>
+          </CardHeader>
+
+          <div className="absolute top-3 right-3">
+            <div className="relative flex h-20 w-20 items-center justify-center">
+              <div className="absolute inset-0 rounded-full bg-purple-300/30 animate-[pulse-ring-expand_2s_ease-in-out_infinite]" />
+              <div className="absolute inset-0 rounded-full bg-purple-300/20 animate-[pulse-ring-expand_2s_ease-in-out_infinite_1s]" />
+              <div className="relative z-10 flex h-20 w-20 items-center justify-center rounded-full bg-purple-300/10">
+                <CarIcon className="h-10 w-10 text-purple-300" />
+              </div>
+            </div>
+          </div>
+        </Card>
+      </Link>
+
+      <Link to="/transport-cards" className="col-span-7">
+        <Card className="rounded-2xl shadow-xl relative overflow-hidden">
+          <CardHeader>
+            <CardTitle className="">Транспортные карты</CardTitle>
+            <CardDescription className="">Управление транспортными картами</CardDescription>
+          </CardHeader>
+
+          <div className="absolute top-3 right-3">
+            <div className="relative flex h-20 w-20 items-center justify-center">
+              <div className="absolute inset-0 rounded-full bg-green-300/30 animate-[pulse-ring-expand_2s_ease-in-out_infinite]" />
+              <div className="absolute inset-0 rounded-full bg-green-300/20 animate-[pulse-ring-expand_2s_ease-in-out_infinite_1s]" />
+              <div className="relative z-10 flex h-20 w-20 items-center justify-center rounded-full bg-green-300/10">
+                <CreditCardIcon className="h-10 w-10 text-green-300" />
+              </div>
+            </div>
+          </div>
+        </Card>
+      </Link>
+
+      <Link to="/calendar" className="col-span-12">
+        <Card className="rounded-2xl shadow-xl relative overflow-hidden">
+          <CardHeader>
+            <CardTitle className="">Календарь доставок</CardTitle>
+            <CardDescription className="max-w-[80%]">
+              Многофункциональный календарь с возможностью просмотра предстоящик доставок, а так же
+              их создание и редактирование
+            </CardDescription>
+          </CardHeader>
+
+          <div className="absolute top-3 right-3">
+            <div className="relative flex h-20 w-20 items-center justify-center">
+              <div className="absolute inset-0 rounded-full bg-cyan-300/30 animate-[pulse-ring-expand_2s_ease-in-out_infinite]" />
+              <div className="absolute inset-0 rounded-full bg-cyan-300/20 animate-[pulse-ring-expand_2s_ease-in-out_infinite_1s]" />
+              <div className="relative z-10 flex h-20 w-20 items-center justify-center rounded-full bg-cyan-300/10">
+                <CalendarDaysIcon className="h-10 w-10 text-cyan-300" />
               </div>
             </div>
           </div>
