0df99ac0a4bf065806b14511c9e38ac5b9dd4112
Никита Водеников <VodenikovNI@mosreg.ru>
2026-07-01 14:04:35 +0300

    feat: roles list page redesign

--- frontend/src/pages/roles/list/index.tsx
+++ frontend/src/pages/roles/list/index.tsx
@@ -5,11 +5,14 @@ import { Role } from "@/lib/types";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DataTable } from "@/components/ui/table";
-import { HomeIcon, MenuIcon } from "lucide-react";
+import { ChevronDown, ChevronUp, HomeIcon, MenuIcon } from "lucide-react";
 import { useIsMobile } from "@/hooks";
 import { useTabbar } from "@/lib/contexts/tabbar-context";
+import { useState } from "react";
 
 export const RolesPage = () => {
+  const [hideBottomTabbar, setHideBottomTabbar] = useState(false);
+
   const navigate = useNavigate();
   const isMobile = useIsMobile();
   const { setOpen } = useTabbar();
@@ -30,40 +33,36 @@ export const RolesPage = () => {
 
   return (
     <div className="flex flex-col gap-4">
-      <Card>
+      <Card className="rounded-2xl shadow-xl">
         <CardHeader>
-          <div className="flex flex-col gap-2">
-            <CardTitle>Роли</CardTitle>
-
-            <div className="flex items-center gap-2">
-              <span className="text-sm text-black">Список ролей</span>
-            </div>
-          </div>
+          <CardTitle>Роли</CardTitle>
         </CardHeader>
       </Card>
 
-      <Card>
-        <CardHeader></CardHeader>
-        <CardContent>
-          {isLoading ? (
-            <div className="text-center py-8 text-muted-foreground">Загрузка...</div>
-          ) : (
-            <DataTable
-              columns={columns}
-              data={roles}
-              onRowClick={(row) =>
-                navigate({ to: "/roles/$roleId", params: { roleId: row.id.toString() } })
-              }
-            />
-          )}
-        </CardContent>
-      </Card>
+      {isLoading ? (
+        <div className="text-center py-8 text-muted-foreground">Загрузка...</div>
+      ) : (
+        <DataTable
+          columns={columns}
+          data={roles}
+          onRowClick={(row) =>
+            navigate({ to: "/roles/$roleId", params: { roleId: row.id.toString() } })
+          }
+        />
+      )}
 
       <div
-        className={`fixed ${isMobile ? "bottom-2" : "bottom-8"} left-1/2 -translate-x-1/2 flex gap-2 p-2 bg-zinc-800/80 rounded-md`}
+        className={`fixed transition-all ${isMobile ? "bottom-2" : hideBottomTabbar ? "-bottom-14" : "bottom-4"} left-1/2 -translate-x-1/2 flex gap-3 p-3 bg-zinc-600/30 backdrop-blur-md shadow-xl border-zinc-200 rounded-2xl`}
       >
+        <div
+          onClick={() => setHideBottomTabbar(false)}
+          className={`absolute -top-4 left-1/2 -translate-x-1/2 px-1 pb-2 bg-[rgb(194,194,197)] rounded-2xl hover:bg-[rgb(173,173,176)] flex items-center justify-center cursor-pointer z-10 transition-all ${hideBottomTabbar ? "opacity-100" : "opacity-0"}`}
+        >
+          <ChevronUp className="text-white w-5" />
+        </div>
+
         <Link to="/">
-          <Button type="button" className="px-3 py-4 bg-zinc-800 rounded-md hover:bg-zinc-900">
+          <Button type="button" className="px-3 py-4 bg-zinc-500/90 rounded-2xl hover:bg-zinc-600">
             <HomeIcon className="w-4 h-4" />
           </Button>
         </Link>
@@ -80,11 +79,22 @@ export const RolesPage = () => {
 
         {canCreate && (
           <Link to="/roles/new">
-            <Button type="button" className="px-8 py-4 bg-blue-600 rounded-md hover:bg-blue-700">
-              Создать роль
+            <Button
+              type="button"
+              className="px-8 py-4 bg-blue-500/90 rounded-2xl hover:bg-blue-600"
+            >
+              Создать
             </Button>
           </Link>
         )}
+
+        <Button
+          onClick={() => setHideBottomTabbar(true)}
+          type="button"
+          className="px-3 py-4 bg-zinc-500/90 rounded-2xl hover:bg-zinc-600"
+        >
+          <ChevronDown className="w-4 h-4" />
+        </Button>
       </div>
     </div>
   );
